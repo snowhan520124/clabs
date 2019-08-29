7609a38b4c7168edd753396a9f6fbba938f3eb8d
Shenshen Han <hanaldo520124@gmail.com>
2019-08-29 14:54:19 -0700

    Style project list.

---
+++ test1/js/app.js
@@ -0,0 +1,10 @@
+$("#test_b1").click(function () {
+    console.log("hi");
+    $("#tab2").css("display", "none");
+    $("#tab3").css("display", "none");
+    $("#tab4").css("display", "none");
+    $("#tab1").css("display", "block");
+    console.log("css");
+});
+
+
