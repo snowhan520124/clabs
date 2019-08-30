c87f44234d6bb7cdbf1d88fadf2d46f6f6ae5a2c
Shenshen Han <hanaldo520124@gmail.com>
2019-08-29 17:36:43 -0700

    Add Vue.

--- test1/js/app.js
+++ test1/js/app.js
@@ -1,22 +1,64 @@
-$("#test_b1").click(function () {
-    console.log("hi");
+var app = new Vue({
+    el: "#app",
+    data: {
+        showOneProject: [false, false, false, false]
+    },
+    methods: {
+        doShowOneProject(index) {
+            var self = this;
+            if (index < 0) {
+                this.showOneProject.forEach(function (item, index) {
+                    Vue.set(self.showOneProject, index, false);
+                });
+                console.log("showAll");
+            } else {
+                Vue.set(self.showOneProject, index, true);
+            }
+            console.log("show " + index);
+        }
+    }
+});
+
+$("#goTopProject1").click(function () {
+    $("#tabMenuButton1").removeClass("active");
+    $("#tabMenuButton2").removeClass("active");
+    $("#tabMenuButton3").removeClass("active");
+    $("#tabMenuButton4").removeClass("active");
     $("#tab2").css("display", "none");
     $("#tab3").css("display", "none");
     $("#tab4").css("display", "none");
     $("#tab1").css("display", "block");
-    console.log("css");
+    app.doShowOneProject(0);
 });
 
-$("#link_top").on('click', function (e) {
+$("#goTopProject2").click(function () {
+    $("#tabMenuButton1").removeClass("active");
+    $("#tabMenuButton2").removeClass("active");
+    $("#tabMenuButton3").removeClass("active");
+    $("#tabMenuButton4").removeClass("active");
+    $("#tab1").css("display", "none");
+    $("#tab2").css("display", "none");
+    $("#tab3").css("display", "none");
+    $("#tab4").css("display", "block");
+    app.doShowOneProject(3);
+});
+
+$("#tabMenuButton1").click(function () {
+    app.doShowOneProject(-1);
+});
+$("#tabMenuButton2").click(function () {
+    app.doShowOneProject(-1);
+});
+$("#tabMenuButton3").click(function () {
+    app.doShowOneProject(-1);
+});
+$("#tabMenuButton4").click(function () {
+    app.doShowOneProject(-1);
+});
+
+$("#link_top").on("click", function (e) {
     e.preventDefault();
-    $('html, body').animate({scrollTop: "0px"}, 500, 'linear');
-});
-
-new Vue({
-    el: "#app",
-    data: {
-        message: "hi"
-    }
+    $("html, body").animate({scrollTop: "0px"}, 500, "linear");
 });
 
 
